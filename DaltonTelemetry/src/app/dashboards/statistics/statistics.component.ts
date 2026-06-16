6f265582c6e7039a8114a3bc5aee706e3c87c36b
Stufy21 <thomas.stufano@gmail.com>
2026-06-16 23:53:30 +0200

    chore: code clean-up

--- DaltonTelemetry/src/app/dashboards/statistics/statistics.component.ts
+++ DaltonTelemetry/src/app/dashboards/statistics/statistics.component.ts
@@ -6,11 +6,7 @@ import { ErgastCircuit } from '../../core/ergast/ergast.model';
 import { ErgastService } from '../../core/ergast/ergast.service';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
-
-export interface TrackUrl {
-  circuitName: string;
-  photoUrl: string | null;
-}
+import { CIRCUIT_SVG_MAP } from '../../consts/CircuitSvgMap';
 
 @Component({
   selector: 'app-statistics',
@@ -20,39 +16,9 @@ export interface TrackUrl {
 })
 export class StatisticsComponent implements OnInit {
   private ergast = inject(ErgastService);
-  // Nel tuo component/service, mappa circuitId → slug del repo
-  private readonly circuitSvgMap: Record<string, string> = {
-    'monza':          'monza-7',
-    'monaco':         'monaco-6',
-    'silverstone':    'silverstone-8',
-    'spa':            'spa-francorchamps-4',
-    'bahrain':        'bahrain-1',
-    'jeddah':         'jeddah-1',
-    'albert_park':    'melbourne-2',
-    'suzuka':         'suzuka-2',
-    'shanghai':       'shanghai-1',
-    'miami':          'miami-1',
-    'imola':          'imola-3',
-    'villeneuve':     'montreal-6',
-    'red_bull_ring':  'spielberg-3',
-    'hungaroring':    'hungaroring-3',
-    'zandvoort':      'zandvoort-5',
-    'baku':           'baku-1',
-    'marina_bay':     'marina-bay-4',
-    'americas':       'austin-1',
-    'rodriguez':      'mexico-city-3',
-    'interlagos':     'interlagos-2',
-    'vegas':          'las-vegas-1',
-    'losail':         'lusail-1',
-    'yas_marina':     'yas-marina-2',
-    'madring':        'madring-1',
-    'catalunya':      'catalunya-6'
-  };
-
 
   circuits = signal<ErgastCircuit[]>([]);
   loading = signal(true);
-  url = signal<TrackUrl[]>([]);
 
   ngOnInit(): void {
     forkJoin([this.ergast.getCircuits(), this.ergast.getRaces()]).subscribe({
@@ -70,7 +36,7 @@ export class StatisticsComponent implements OnInit {
   }
 
   getCircuitSvgUrl(circuitId: string): string {
-    const slug = this.circuitSvgMap[circuitId] ?? circuitId;
+    const slug = CIRCUIT_SVG_MAP[circuitId] ?? circuitId;
     return `https://raw.githubusercontent.com/julesr0y/f1-circuits-svg/refs/heads/main/circuits/minimal/black-outline/${slug}.svg`;
   }
 }
